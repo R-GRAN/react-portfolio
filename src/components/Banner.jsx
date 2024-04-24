4574df3c5ef5482453d3f624d03a8c83d5bd83fc
Remy <remy.grangenois@gmail.com>
2024-04-24 17:12:53 +0200

    img contenair

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,6 @@ function Banner() {
     <div className="banner" id="home">
       <img
         src="https://images.unsplash.com/photo-1532529867795-3c83442c1e5c?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        onError={mac_open}
         alt="bannière"
         className="banner-background"
       />
