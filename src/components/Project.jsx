e4418b3b7668b8eaac4b6f9cfd6ed1acb12b0a96
Remy <remy.grangenois@gmail.com>
2024-04-26 18:50:04 +0200

    animation hover img

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,14 +1,18 @@
 function Project({ project, index }) {
   return (
     <article className={index % 2 === 0 ? "project" : "project light-reverse"}>
+<a href={project.githubUrl} target="_blank" rel="noopener noreferrer" title="Consulter le code sur GitHub">
       <div className="project-img-container">
         <img
           className="project-img"
           src={project.imageUrl}
           alt={"visuel du projet " + project.title}
         />
+        <div className="project-title">
+          <span>Consulter le code sur GitHub</span>
+        </div>
       </div>
-
+      </a>
       <div className="project-block">
         <h4>{project.title}</h4>
         <p>{project.category}</p>
@@ -18,6 +22,7 @@ function Project({ project, index }) {
           Consulter le code
         </a>
       </div>
+
     </article>
   );
 }
