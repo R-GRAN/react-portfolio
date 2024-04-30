16a1563fb8e1f8fe3768726a23eaa6898bc200b0
Remy <remy.grangenois@gmail.com>
2024-05-01 00:17:18 +0200

    formAuth+ react router Dom

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "@/pages/Home/index.jsx";
 import "@/assets/styles/main.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "@/pages/Login/index.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
